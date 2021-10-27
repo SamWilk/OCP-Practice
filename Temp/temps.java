25582855970a55a61f01cc0591bb3803e7ebf8be
SamWilk <samwilk18@yahoo.com>
2021-10-26 23:06:35 -0400

    Just testing things out in VS code

---
+++ Temp/temps.java
@@ -0,0 +1,9 @@
+package Temp;
+
+public class temps{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        jokes flag = new jokes();
+        flag.display();
+    } 
+}
